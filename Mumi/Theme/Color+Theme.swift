31708225592d9cffade743e57d1ab47611e21c01
SU WEILONG <stinge62580@gmail.com>
2025-06-30 04:27:43 +0900

    fix(theme): Rename color assets to avoid conflicts

--- Mumi/Theme/Color+Theme.swift
+++ Mumi/Theme/Color+Theme.swift
@@ -5,11 +5,11 @@ extension Color {
         static var background: Color { Color("Background") }
         static var text: Color { Color("Text") }
         static var accent: Color { Color("Accent") }
-        static var primary: Color { Color("Primary") }
+        static var primary: Color { Color("ThemePrimary") }
         static var onPrimary: Color { Color("OnPrimary") }
         static var surface: Color { Color("Surface") }
         static var onSurface: Color { Color("OnSurface") }
-        static var secondary: Color { Color("Secondary") }
+        static var secondary: Color { Color("ThemeSecondary") }
         static var onSecondary: Color { Color("OnSecondary") }
         static var error: Color { Color("Error") }
         static var onError: Color { Color("OnError") }
