f95ff81ce7508e57c10d36e6d410afb2e91ae7b2
SU WEILONG <stinge62580@gmail.com>
2025-06-30 03:08:11 +0900

    feat(theme): Implement Modern Digital color theme (Option 2)

---
+++ Mumi/Theme/Color+Theme.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+extension Color {
+    struct Theme {
+        static var background: Color {
+            Color("Background")
+        }
+        static var text: Color {
+            Color("Text")
+        }
+        static var accent: Color {
+            Color("Accent")
+        }
+    }
+}
