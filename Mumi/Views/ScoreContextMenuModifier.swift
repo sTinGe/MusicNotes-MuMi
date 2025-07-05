daaab35ddc380f10e2eebc90e4eb73b1bf435eb7
SU WEILONG <stinge62580@gmail.com>
2025-07-06 07:19:43 +0900

    add swiftlint

---
+++ Mumi/Views/ScoreContextMenuModifier.swift
@@ -0,0 +1,58 @@
+//
+//  ScoreContextMenuModifier.swift
+//  Mumi
+//
+//  Created by stinge on 2025/07/06.
+//
+
+import SwiftUI
+
+struct ScoreContextMenuModifier: ViewModifier {
+    let score: Score
+    @ObservedObject var viewModel: ScoreLibraryViewModel
+    @Binding var scoreToRename: Score?
+    @Binding var isRenameSheetPresented: Bool
+
+    func body(content: Content) -> some View {
+        content
+            .contextMenu {
+                Button {
+                    scoreToRename = score
+                    isRenameSheetPresented = true
+                } label: {
+                    Label(
+                        "Rename",
+                        systemImage: "pencil"
+                    )
+                    .accessibilityIdentifier(AccessibilityIdentifiers.scoreThumbnailMenuButtonRenameLabel)
+                }
+                Button(role: .destructive) {
+                    viewModel.deleteScore(score)
+                } label: {
+                    Label(
+                        "Delete",
+                        systemImage: "trash"
+                    )
+                    .accessibilityIdentifier(AccessibilityIdentifiers.scoreThumbnailMenuButtonDeleteLabel)
+                }
+            }
+    }
+}
+
+extension View {
+    func scoreContextMenu(
+        score: Score,
+        viewModel: ScoreLibraryViewModel,
+        scoreToRename: Binding<Score?>,
+        isRenameSheetPresented: Binding<Bool>,
+    ) -> some View {
+        self.modifier(
+            ScoreContextMenuModifier(
+                score: score,
+                viewModel: viewModel,
+                scoreToRename: scoreToRename,
+                isRenameSheetPresented: isRenameSheetPresented
+            )
+        )
+    }
+}
