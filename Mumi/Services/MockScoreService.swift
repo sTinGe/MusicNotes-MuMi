8cd60b713303988faad80a016bd784e8af693d48
SU WEILONG <stinge62580@gmail.com>
2025-07-13 23:38:07 +0900

    add control_panel.md

--- Mumi/Services/MockScoreService.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-class MockScoreService: ScoreService {
-    override func fetchScores() -> [Score] {
-        let dummyURL = URL(fileURLWithPath: "/dev/null")
-        return [Score(filename: "dummy.pdf", url: dummyURL)]
-    }
-}
